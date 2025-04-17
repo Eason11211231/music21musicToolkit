273d7290ae014ff1ba8b0eb1a8fd3b1ee7de4aea
Eason11211231 <easonfff@gmail.com>
2025-04-17 09:36:17 +0800

    Update generate_utils.py

--- generate_utils.py
+++ generate_utils.py
@@ -4,17 +4,23 @@ import dropbox
 import os
 import uuid
 
-DROPBOX_ACCESS_TOKEN = "你的 token 放這裡"
+# 請在這裡貼入你的 Dropbox token
+DROPBOX_ACCESS_TOKEN = "sl.xxxxx.your-new-token"
 dbx = dropbox.Dropbox(DROPBOX_ACCESS_TOKEN)
 
 def generate_melody(style, key_str, time_signature, measures=8):
     score = stream.Score()
 
-    # 調性 & 拍號
-    k = m21key.Key(key_str)
-    ts = meter.TimeSignature(time_signature)
+    try:
+        k = m21key.Key(key_str)
+    except:
+        k = m21key.Key("C")
 
-    # 建立兩個獨立聲部
+    try:
+        ts = meter.TimeSignature(time_signature)
+    except:
+        ts = meter.TimeSignature("4/4")
+
     melody = stream.Part()
     melody.id = "melody"
     melody.append(k)
@@ -25,34 +31,29 @@ def generate_melody(style, key_str, time_signature, measures=8):
     bass.append(k)
     bass.append(ts)
 
-    # 產生旋律 & 低音音符
     scale_pitches = k.getPitches()
 
-for _ in range(measures):
-    m1 = stream.Measure()
-    m2 = stream.Measure()
+    for _ in range(measures):
+        m1 = stream.Measure()
+        m2 = stream.Measure()
 
-    for _ in range(4):  # 每拍都產生旋律與低音
-        # 旋律音
-        pitch_m = random.choice(scale_pitches)
-        n1 = note.Note(pitch_m)
-        n1.quarterLength = 1.0
-        m1.append(n1)
+        for _ in range(4):  # 每拍
+            pitch_m = random.choice(scale_pitches)
+            n1 = note.Note(pitch_m)
+            n1.quarterLength = 1.0
+            m1.append(n1)
 
-        # 低音音（下八度）
-        pitch_b = random.choice(scale_pitches).transpose(-12)
-        n2 = note.Note(pitch_b)
-        n2.quarterLength = 1.0
-        m2.append(n2)
+            pitch_b = random.choice(scale_pitches).transpose(-12)
+            n2 = note.Note(pitch_b)
+            n2.quarterLength = 1.0
+            m2.append(n2)
 
-    melody.append(m1)
-    bass.append(m2)
-
+        melody.append(m1)
+        bass.append(m2)
 
     score.append(melody)
     score.append(bass)
 
-    # 儲存並上傳到 Dropbox
     filename = f"melody_{uuid.uuid4().hex}.musicxml"
     score.write('musicxml', fp=filename)
 
